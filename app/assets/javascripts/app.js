5d5cc8fab15d8671291e43e3aa081b7bd986bb30
raemilla <rachael.miller05@gmail.com>
2017-01-14 15:42:11 -0500

    ajax adding comments

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  $(".comment-form").on("submit", function(event){
+    event.preventDefault();
+    $.ajax({
+      method: "post",
+      url: "/comments",
+      data: $(event.target).serialize()
+      // {
+      //   author: $(".comment-author").val(),
+      //   text: $(".comment-text").val(),
+      //   post_id: $(".post-id").val()
+      // }
+    }).done(function(response){
+      $(".comment-author").val("");
+      $(".comment-text").val("");
+      $(".comment-container").append("<p>"+response.text+"</p><p>posted by "+response.author+" on "+response.created_at+"</p>");
+    })
+  })
+})
